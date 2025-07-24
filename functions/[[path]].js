4c3c70335a3ef74683af88eaf5c44e38ad670436
Limkon <limkon@skvip.eu.org>
2025-07-24 10:04:54 +0800

    Delete functions directory

--- functions/[[path]].js
+++
@@ -1,4 +0,0 @@
-import { createPagesFunctionHandler } from '@cloudflare/workers-express-adapter';
-import app from '../server'; // 導入我們修改後的 Express app
-
-export const onRequest = createPagesFunctionHandler({ app });
